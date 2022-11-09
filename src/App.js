c56c5168e0a9e2d3d01a0daf81bc78502a626502
David <siddav83@gmail.com>
2022-11-09 15:55:19 +0000

    jsx setup in User and created nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Home, Repo, User, UserRepos} from './pages'
+import { Navbar } from './components';
 import { Routes, Route} from 'react-router-dom';
 import './styles/App.css';
 import axios from 'axios'
+
 
 function searchRepos() {
 
     axios({
         method: 'get',
-        url: `https://api.github.com/users/siddav83/repos`,
+        url: `https://api.github.com/users/siddav83`,
     }).then(res => {
         // setLoading(false);
         // setRepos(res.data);
@@ -18,12 +20,14 @@ function searchRepos() {
 searchRepos()
 
 function App() {
+const [searchTerm, setSearchTerm] = useState([])
+
   return (
       <div id="app">
           <main>
               <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/user" element={<User />} />
+                  <Route path="/" element={<Home searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>} />
+                  <Route path="/user" element={<User navbar={Navbar} />} />
                   <Route path="/user/repos" element={<UserRepos/>} />
                   <Route path="/user/repos/repo" element={<Repo/>} />
               </Routes>
