dd16b7114a0c68a0c3304e12976622fe3dda2666
GimmeDaLute <peter.m.edwards@outlook.com>
2022-11-09 10:56:32 +0000

    Added pages for app. Added webpack + babel + dependencies, some missing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
-import './App/App.css';
+import React from 'react';
+import { Home, Repo, User, UserRepos} from './pages'
+import { Routes, Route} from 'react-router-dom';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+      <div id="app">
+          <main>
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/user" element={<User />} />
+                  <Route path="/user/repos" element={<UserRepos/>} />
+                  <Route path="/user/repos/repo" element={<Repo/>} />
+              </Routes>
+          </main>
+      </div>
+  )
 }
 
 export default App;
