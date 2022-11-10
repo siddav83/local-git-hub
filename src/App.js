cd29a6d64f2129e1af7ee4aad11e62a865d0502c
David <siddav83@gmail.com>
2022-11-10 15:49:54 +0000

    updating pages with data

--- src/App.js
+++ src/App.js
@@ -5,29 +5,32 @@ import { FourOhFour, Home, Repo, User, UserRepos} from './pages'
 import './styles/App.css';
 
 function App() {
-const [userName, setUsername] = useState("shit");
-const [searchTerm,setSearchTerm] = useState("poo");
-const [data,setData] = useState([]);
+const [userName, setUsername] = useState("");
+const [searchTerm,setSearchTerm] = useState("");
+const [repoData,setRepoData] = useState([]);
 
 useEffect(() => {
+  
     const getData = async () => {
-        try {
-            const response = await fetch(`https://api.github.com/users/${searchTerm}/repos`)
+ 
+            const response = await fetch(`https://api.github.com/users/${searchTerm}`)
+
             const data = await response.json()
-         setData(data)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-    getData()
+
+       setRepoData(data);
+    
+            }
+              getData().catch(console.error)
+
+
     }, [searchTerm])
 
   return (
       <div id="app">
           <main>
               <Routes>
-                  <Route path="/" element={<Home userName={userName} setUsername={setUsername} searchTerm={searchTerm} setSearchTerm={setSearchTerm} setData={setData} />} />
-                  <Route path="/user" element={<User navbar={Navbar} data={data} />} />
+                  <Route path="/" element={<Home userName={userName} setUsername={setUsername} searchTerm={searchTerm} setSearchTerm={setSearchTerm} setData={setRepoData} data={repoData}/>} />
+                  <Route path="/user" element={<User navbar={Navbar} data={repoData} />} />
                   <Route path="/user/repos" element={<UserRepos/>} />
                   <Route path="/user/repos/repo" element={<Repo/>} />
                   <Route path="*" element={<FourOhFour/>} />
